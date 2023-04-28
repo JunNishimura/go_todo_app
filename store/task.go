d3db80ba4afff34ee88cb6829ebbca3092ae042b
Jun Nishimura <n.junjun0303@gmail.com>
2023-04-28 23:49:14 +0900

    add task_test

--- store/task.go
+++ store/task.go
@@ -21,3 +21,12 @@ func (r *Repository) AddTask(ctx context.Context, db Execer, t *entity.Task) err
 	t.ID = entity.TaskID(id)
 	return nil
 }
+
+func (r *Repository) ListTasks(ctx context.Context, db Queryer) (entity.Tasks, error) {
+	tasks := entity.Tasks{}
+	sql := `SELECT id, title, status, created, modified FROM task;`
+	if err := db.SelectContext(ctx, &tasks, sql); err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
